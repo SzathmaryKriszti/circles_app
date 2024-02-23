c4ea230264598a4590b022d9ea5d3952a44a6d66
SzathmaryKriszti <krisztacsuri@gmail.com>
2024-02-23 12:42:24 +0100

    Config Routing

--- frontend/src/app/routing/app-routing.module.ts
+++ frontend/src/app/routing/app-routing.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {Routes} from "@angular/router";
+import {RouterModule, Routes} from "@angular/router";
 import {RegistrationComponent} from "../component/registration/registration.component";
 import {WelcomeComponent} from "../component/welcome/welcome.component";
+import {LoginComponent} from "../component/login/login.component";
 
 const routes: Routes = [
-  {path: '', component: WelcomeComponent},
-  {path: 'registration', component: RegistrationComponent}
+  {path: '', component: WelcomeComponent, pathMatch: 'full'},
+  {path: 'registration', component: RegistrationComponent, pathMatch: 'full'},
+  {path: 'login', component: LoginComponent, pathMatch: 'full'}
 ];
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
