b78e5a4315bc9fa8ea8771e50e52f557dd70d745
SzathmaryKriszti <krisztacsuri@gmail.com>
2024-02-23 11:21:24 +0100

    Change App name in .java file

--- src/main/java/hu/progmasters/thefriendzoneapp/CirclesApplication.java
+++ src/main/java/hu/progmasters/thefriendzoneapp/CirclesApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TheFriendzoneApplication {
+public class CirclesApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TheFriendzoneApplication.class, args);
+        SpringApplication.run(CirclesApplication.class, args);
     }
 
 }
