f3e9a48255a228e668eeffeb34e674007ac9955e
SzathmaryKriszti <krisztacsuri@gmail.com>
2024-03-11 13:58:39 +0100

    fix authentication

--- src/main/java/hu/progmasters/circlesapp/config/SecurityConfig.java
+++ src/main/java/hu/progmasters/circlesapp/config/SecurityConfig.java
@@ -1,7 +1,7 @@
-package hu.progmasters.circlesapp.security;
+package hu.progmasters.circlesapp.config;
 
+import hu.progmasters.circlesapp.domain.UserRole;
 import hu.progmasters.circlesapp.service.AppUserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -13,32 +13,36 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 
-@Configuration
 @EnableWebSecurity
+@Configuration
 public class SecurityConfig {
 
-    private AppUserService appUserService;
+    private final AppUserService appUserService;
 
 
-    @Autowired
     public SecurityConfig(AppUserService appUserService) {
         this.appUserService = appUserService;
-
     }
 
-
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
+
         http
                 .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(Customizer.withDefaults())
-
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(HttpMethod.POST,"/api/users/registration").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/users")
+                        .hasRole(UserRole.ROLE_USER.getDisplayRole())
+                )
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "/api/users/registration").permitAll()
                 )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.GET, "/api/users/login").permitAll()
+                )
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.GET, "/api/users/logout").permitAll()
                 )
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().denyAll()
@@ -51,9 +55,6 @@ public class SecurityConfig {
                         .clearAuthentication(true)
                 )
                 .userDetailsService(appUserService);
-
-
         return http.build();
-
     }
 }
