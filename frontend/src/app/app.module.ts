2f83f4cecec5aee3fb5e2ecd01fdfda7aa86bfda
SzathmaryKriszti <krisztacsuri@gmail.com>
2024-04-02 15:32:18 +0200

    -fix deploy

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { UserProfileComponent } from './component/user-profile/user-profile.comp
 import {AuthInterceptor} from "./utils/auth.interceptor";
 import { GroupFormComponent } from './component/group-form/group-form.component';
 import { MyGroupsComponent } from './component/my-groups/my-groups.component';
+import { FindGroupComponent } from './component/find-group/find-group.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { MyGroupsComponent } from './component/my-groups/my-groups.component';
     LoginComponent,
     UserProfileComponent,
     GroupFormComponent,
-    MyGroupsComponent
+    MyGroupsComponent,
+    FindGroupComponent
   ],
   imports: [
     BrowserModule,
