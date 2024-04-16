13f306c175a8892b58571bc00a77992a80efdd7c
SzathmaryKriszti <krisztacsuri@gmail.com>
2024-04-16 15:00:15 +0200

    add order by param

--- src/main/java/hu/progmasters/circlesapp/repository/GroupRepository.java
+++ src/main/java/hu/progmasters/circlesapp/repository/GroupRepository.java
@@ -11,7 +11,15 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface GroupRepository extends JpaRepository<Group, Long> {
 
-    @Query("SELECT g FROM Group g JOIN g.members m LEFT JOIN g.posts p LEFT JOIN g.events e WHERE m = :user GROUP BY g ORDER BY (COUNT(p) + COUNT(e)) DESC")
+    @Query("SELECT g FROM Group g " +
+            "JOIN g.members m " +
+            "LEFT JOIN g.posts p " +
+            "LEFT JOIN g.events e " +
+            "WHERE m = :user " +
+            "GROUP BY g " +
+            "ORDER BY g.createdAt DESC"
+//            "ORDER BY (COUNT(p) + COUNT(e)) DESC"
+           )
     Page<Group> findGroupsJoinedByUser(AppUser user, Pageable pageable);
 
     @Query("SELECT g FROM Group g WHERE :user NOT MEMBER OF g.members")
