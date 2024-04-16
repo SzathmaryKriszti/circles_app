9f5e19996ee852ea25eb824e0c34c7f72ae2a74d
SzathmaryKriszti <krisztacsuri@gmail.com>
2024-04-16 15:01:37 +0200

    refactor

--- src/main/java/hu/progmasters/circlesapp/service/GroupService.java
+++ src/main/java/hu/progmasters/circlesapp/service/GroupService.java
@@ -22,6 +22,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -57,7 +58,7 @@ public class GroupService {
         Page<Group> currentPage = groupRepository.findGroupsJoinedByUser(user, pageable);
         List<GroupListItem> groups = currentPage.stream()
                 .map(group -> new GroupListItem(user, group))
-                .toList();
+                .collect(Collectors.toList());
         return new JoinedGroupList(currentPage.getTotalPages(), groups);
     }
 
