7f341ab9a02695051f85757e56c4362d1ed29490
SzathmaryKriszti <krisztacsuri@gmail.com>
2024-04-17 12:54:05 +0200

    add routing for group page

--- frontend/src/app/routing/app-routing.module.ts
+++ frontend/src/app/routing/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
 import {RegistrationComponent} from "../component/registration/registration.component";
-import {WelcomeComponent} from "../component/welcome/welcome.component";
 import {LoginComponent} from "../component/login/login.component";
 import {UserProfileComponent} from "../component/user-profile/user-profile.component";
 import {GroupFormComponent} from "../component/group-form/group-form.component";
 import {MyGroupsComponent} from "../component/my-groups/my-groups.component";
 import {FindGroupComponent} from "../component/find-group/find-group.component";
 import {SearchGroupComponent} from "../component/search-group/search-group.component";
+import {GroupComponent} from "../component/group/group.component";
 
 
 const routes: Routes = [
@@ -20,6 +20,7 @@ const routes: Routes = [
   {path: 'my-groups', component: MyGroupsComponent, pathMatch: 'full'},
   {path: 'find-group', component: FindGroupComponent, pathMatch: 'full'},
   {path: 'search-group', component: SearchGroupComponent, pathMatch: 'full'},
+  {path: 'group/:id', component: GroupComponent, pathMatch: 'full'},
 ];
 
 @NgModule({
