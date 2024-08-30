25cc53465fcbde6334eb17e52d2057be57f7b1fd
SzathmaryKriszti <krisztacsuri@gmail.com>
2024-08-30 10:22:48 +0200

    modify test class

--- src/test/java/hu/progmasters/circlesapp/MyFirstAppApplicationTests.java
+++ src/test/java/hu/progmasters/circlesapp/MyFirstAppApplicationTests.java
@@ -1,9 +1,8 @@
 package hu.progmasters.circlesapp;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class MyFirstAppApplicationTests {
 
     @Test
